20019f2fe6768a3cbd917c0ed87c7267c9b35280
raquel <raquelanaroman@gmail.com>
2021-07-01 16:46:50 -0400

    update styling

--- src/components/statbar/StatBar.js
+++ src/components/statbar/StatBar.js
@@ -3,17 +3,17 @@ import './style.css';
 function StatBar({ stat = 100, statName = "food" }) {
     return (
         <div id="statDiv">
-                <p id="statName">{statName}</p>
-                <div id="statBarContainer" >
-                        <div
-                            id="statDisplay"
-                            style={{
-                                backgroundColor: "red",
-                                width: `${stat}px`,
-                                height: "13px",
-                            }} 
-                        />
-                </div>
+            <p id="statName">{statName}</p>
+            <div id="statBarContainer" >
+                <div
+                    id="statDisplay"
+                    style={{
+                        backgroundColor: "rgb(255, 127, 127)",
+                        width: `${stat}px`,
+                        height: "13px",
+                    }}
+                />
+            </div>
         </div>
     )
 }
