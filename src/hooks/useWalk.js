5e24c88d3505765ec154a3649de881a7645d5ac9
raquel <raquelanaroman@gmail.com>
2021-06-30 22:12:49 -0400

    change hook to reducer

--- src/hooks/useWalk.js
+++ src/hooks/useWalk.js
@@ -1,43 +1,80 @@
-import { useState } from "react";
+import { useReducer } from "react";
+import { MAP_TABLE, PLAYER_FRAMES, DIRECTION_AXIS, PLAYER_SIZE } from "../config/constants";
 
-function useWalk(maxSteps){
-    const [direction, setDirection] = useState(0);
-    const [step, setStep] = useState(0);
-    const [position, setPosition] = useState({x: 0, y: 0});
+const reducer = (state, action) => {
+    switch(action.type){
+        case 'down': 
+            return {
+                direction: 0, 
+                animationFrame: state.animationFrame < PLAYER_FRAMES - 1 ? state.animationFrame + 1 : 0,
+                position: {...state.position, y: state.position.y + 1},
+                tile: {...state.tile, y: state.position.y % 32 === 0 ? state.tile.y + 1 : state.tile.y}
+            }
+        case 'left': 
+            return {
+                direction: 1, 
+                animationFrame: state.animationFrame < PLAYER_FRAMES - 1 ? state.animationFrame + 1 : 0,
+                position: {...state.position, x: state.position.x - 1},
+                tile: {...state.tile, x: state.position.x % 32 === 0 ? state.tile.x - 1 : state.tile.x}
+            }
+        case 'right': 
+            return {
+                direction: 2, 
+                animationFrame: state.animationFrame < PLAYER_FRAMES - 1 ? state.animationFrame + 1 : 0,
+                position: {...state.position, x: state.position.x + 1},
+                tile: {...state.tile, x: state.position.x % 32 === 0 ? state.tile.x + 1 : state.tile.x}
+            }
+        case 'up': 
+            return {
+                direction: 3, 
+                animationFrame: state.animationFrame < PLAYER_FRAMES - 1 ? state.animationFrame + 1 : 0,
+                position: {...state.position, y: state.position.y - 1},
+                tile: {...state.tile, y: state.position.y % 32 === 0 ? state.tile.y - 1 : state.tile.y}
+            }
+        case 'bounce':
+            return{
+                ...state,
+                position: {...state.position, x: state.position.x + 1},
+            }
+    }
+}
 
-    const stepSize = 2;
+function useWalk(){
+    const [state, dispatch] = useReducer(reducer, {
+        direction: 0,
+        animationFrame: 0,
+        position: {x:256, y:192},
+        tile: {x:8, y:6}
+    })
 
-    const dirMap ={
-        down: 0,
-        left: 1,
-        right: 2,
-        up: 3,
-    };
+    const { direction, animationFrame, position, tile } = state;
 
-    const movementMap = {
-        down: { x: 0, y: stepSize },
-        left: { x: -stepSize, y: 0 },
-        right: { x: stepSize, y: 0 },
-        up: { x: 0, y: -stepSize },
+    function walk(arrowKey){
+        
+        if(getNextTile(arrowKey)){
+            dispatch({type: arrowKey})
+        }
+
     }
 
-    function walk(direction){
-        setDirection(prevDir => {
-            if(dirMap[direction] === prevDir) getNewPosition(direction);
-            return dirMap[direction];
-        })
-        setStep(step => step < maxSteps - 1 ? step + 1 : 0)
-    }
+    function getNextTile(direction){
+        const currentXCord = (state.position.x - (state.position.x % 32)) / 32
+        const currentYCord = (state.position.y - (state.position.y % 32)) / 32 
 
-    function getNewPosition(direction){
-        setPosition((prev)=> ({
-            x: prev.x + movementMap[direction].x,
-            y: prev.y + movementMap[direction].y,
-        }))
+        switch(direction){
+            case 'down': 
+                return MAP_TABLE[currentYCord + 1][currentXCord]["walk"]
+            case 'left': 
+                return MAP_TABLE[currentYCord][currentXCord]["walk"]
+            case 'right': 
+                return MAP_TABLE[currentYCord][currentXCord + 1]["walk"]
+            case 'up': 
+                return MAP_TABLE[currentYCord][currentXCord]["walk"]
+        }
     }
 
     return {
-        walk, direction, step, position
+        direction, animationFrame, position, tile, walk
     }
 }
 
