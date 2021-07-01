fb736ce6fd8a719b48275401b687bd9c473100df
raquel <raquelanaroman@gmail.com>
2021-07-01 14:47:17 -0400

    edit styles for HUD

--- src/components/hud/HUD.js
+++ src/components/hud/HUD.js
@@ -1,7 +1,18 @@
 import StatBar from "../statbar/StatBar";
+import Clock from "../clock/Clock";
+import './style.css';
 
-function HUD(){
-
+function HUD() {
+    return (
+        <div id='HUDDiv'>
+            <Clock/>
+            <div id='statsContainer'>
+                <StatBar stat={100} statName={"food"} />
+                <StatBar stat={80} statName={"walk"} />
+                <StatBar stat={90} statName={"play"} />
+            </div> 
+        </div>
+    )
 }
 
 export default HUD;
