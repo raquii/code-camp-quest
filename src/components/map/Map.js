98b2e5ef7c7ae33bd0ff636f725c8121d67bc85a
raquel <raquelanaroman@gmail.com>
2021-07-03 20:44:26 -0400

    remove map grid code

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,32 +1,23 @@
-
-// import { MAP_TABLE } from "../../config/constants";
 import MapTile from "./MapTile";
 import './style.css'
 
-function Map({x}) {
-    
-    /* This is for debugging the MAP_TABLE*/
-
-    // const mapGrid = MAP_TABLE.map((row, y)=>(
-    //     <div key = {y} style={{display: "flex"}}>
-    //         {row.map(tile => (
-    //             <div key={tile} style = {{ boxSizing: "border-box", borderTop: "1px solid black", borderRight: "1px solid black", width: "32px", height: "32px", backgroundColor: tile.walk ? "green" : "red", opacity: 0.5}} />
-    //         ))}
-    //     </div>))
+function Map({ x }) {
 
     return (
-        <div id="map"  style={{
-            zIndex:1,
-            position: "absolute",
-            top: 0,
-            left: -x - 10,
-            
-        }}
-            >
+        <div id="map"
+            style={{
+                zIndex: 1,
+                position: "absolute",
+                top: 0,
+                left: -x - 16,
+
+            }}
+        >
+
             <MapTile map={`bedroom`} />
             <MapTile map={`office`} />
             <MapTile map={`kitchen`} />
-       </div>
+        </div>
     )
 }
 
