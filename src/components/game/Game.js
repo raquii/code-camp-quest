a4c4814aa77a494517bf58970d19d16bffb61c09
raquel <raquelanaroman@gmail.com>
2021-07-05 17:48:19 -0400

    add context for message component

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,12 +1,24 @@
 
 import HUD from "../hud/HUD"
 import Viewport from "../viewport/Viewport";
+import Button from "../button/Button";
+import { useState} from "react";
+import { ShowMessage } from "../../utilities/show-message"
 
 function Game() {
+    const [isPaused, setIsPaused] = useState(false);
+
+    function onPauseClick(){
+        setIsPaused(!isPaused)
+    }
+
     return (
         <>
-            <Viewport />
-
+            <ShowMessage>
+                <Viewport />
+            </ShowMessage>
+    
+            <Button label = {isPaused ? '⏸️' : '▶' } handleClick={onPauseClick}/>
             <HUD />
         </>
     )
