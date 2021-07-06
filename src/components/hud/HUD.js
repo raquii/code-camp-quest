c338bbe836bfd27836c64b0a9243b4ccc8b6a9fc
raquel <raquelanaroman@gmail.com>
2021-07-06 15:22:15 -0400

    add stat state from redux store

--- src/components/hud/HUD.js
+++ src/components/hud/HUD.js
@@ -1,16 +1,60 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { decreaseStat, increaseStat } from '../game/gameSlice'
+
 import StatBar from "../statbar/StatBar";
 import Clock from "../clock/Clock";
+
 import './style.css';
 
 function HUD() {
+    const dispatch = useDispatch();
+
+    const paused = useSelector(state => state.rootReducer.game.paused)
+
+    const food = useSelector(state => state.rootReducer.game.stats.foodStat)
+    const walk = useSelector(state => state.rootReducer.game.stats.walkStat)
+    const play = useSelector(state => state.rootReducer.game.stats.playStat)
+
+    useEffect(() => {
+        let foodInt;
+
+        if(paused){
+            clearInterval(foodInt)
+        }else{
+            foodInt =  setInterval(() => {
+                dispatch(decreaseStat('foodStat'))
+            }, 1000)
+        }
+
+        return () => {
+            clearInterval(foodInt)
+        }
+    }, [dispatch, paused])
+
+    useEffect(() => {
+        let walkInt;
+
+        if(paused){
+            clearInterval(walkInt)
+        }else{
+            walkInt =  setInterval(() => {
+                dispatch(decreaseStat('walkStat'))
+            }, 5000)
+        }
+
+        return () => {
+            clearInterval(walkInt)
+        }
+    }, [dispatch, paused])
 
     return (
         <div id='HUDDiv'>
             <Clock />
             <div id='statsContainer'>
-                <StatBar stat={100} statName={"food"} />
-                <StatBar stat={80} statName={"walk"} />
-                <StatBar stat={90} statName={"play"} />
+                <StatBar stat={food} statName={"food"} />
+                <StatBar stat={walk} statName={"walk"} />
+                <StatBar stat={play} statName={"play"} />
             </div>
         </div>
     )
