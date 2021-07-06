1086a498deb479b874a218848d42d74fffce757b
raquel <raquelanaroman@gmail.com>
2021-07-06 15:21:03 -0400

    fix useEffect dependencies

--- src/components/clock/Clock.js
+++ src/components/clock/Clock.js
@@ -7,28 +7,36 @@ import { minuteChange, hourChange, middayToggle, afterNoon } from './clockSlice'
 function Clock() {
     const dispatch = useDispatch();
 
+    const paused = useSelector(state => state.rootReducer.game.paused)
+
     const hour = useSelector(state => state.rootReducer.clock.hour)
     const minute = useSelector(state => state.rootReducer.clock.minute)
     const midday = useSelector(state => state.rootReducer.clock.midday)
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            if (minute < 5) {
-                dispatch(minuteChange())
-            } else if (minute === 5 && hour === 11) {
-                dispatch(middayToggle())
-            } else if (minute === 5 && hour === 12) {
-                dispatch(afterNoon())
-            } else {
-                dispatch(hourChange())
-            }
-        }, 5000)
+        let clockInterval;
 
-        return () => {
-            clearInterval(interval)
+        if(paused){
+            clearInterval(clockInterval)
+        }else{
+            clockInterval = setInterval(() => {
+                if (minute < 5) {
+                    dispatch(minuteChange())
+                } else if (minute === 5 && hour === 11) {
+                    dispatch(middayToggle())
+                } else if (minute === 5 && hour === 12) {
+                    dispatch(afterNoon())
+                } else {
+                    dispatch(hourChange())
+                }
+            }, 1000)
         }
 
-    }, [dispatch, hour, minute])
+        return () => {
+            clearInterval(clockInterval)
+        }
+
+    }, [dispatch, hour, minute, paused])
 
     return <div id='clock'> {hour}:{minute}0 {midday ? 'pm' : 'am'} | Day 1 </div>
 }
