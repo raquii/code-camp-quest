83da536417b0242cc59ba291651e94e8b8d2a3af
raquel <raquelanaroman@gmail.com>
2021-07-06 15:21:31 -0400

    add stat reducers

--- src/components/game/gameSlice.js
+++ src/components/game/gameSlice.js
@@ -1,14 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
+import getDailyTasks from "../../utilities/get-daily-tasks";
 
 const initialState = {
     paused: false,
-    foodStat: 100,
-    walkStat: 100,
-    playStat: 100,
+    stats: {
+        foodStat: 100,
+        walkStat: 100,
+        playStat: 100,
+    },
     taskStat: 0,
     totalTasks: [],
     day: 1,
 };
+
+//need to create daily task array by passing state.day to getDailyTasks
 
 const gameSlice = createSlice({
     name: 'game',
@@ -20,17 +25,21 @@ const gameSlice = createSlice({
         newDay: (state, action) => {
             state.day += 1
             state.totalTasks = action.payload
-
         },
-        decreaseStats: state => {
-            state.foodStat -= 1
+        decreaseStat: (state, action) => {
+            const stat = action.payload
+            state.stats[stat] -= 1
+        },
+        increaseStat: (state, action) => {
+            const stat = action.payload
+            state.stats[stat] += 1
         }
     }
-    
+
 })
 
-const { togglePaused } = gameSlice.actions;
+const { togglePaused, newDay, decreaseStat, increaseStat } = gameSlice.actions;
 
-export { togglePaused }
+export { togglePaused, newDay, decreaseStat, increaseStat }
 
 export default gameSlice.reducer
