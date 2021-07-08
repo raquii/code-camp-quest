ff1a2c04e36d9cc13603a936892448ff0c3efcc5
raquel <raquelanaroman@gmail.com>
2021-07-08 00:34:24 -0400

    add alert for low stats

--- src/components/dog/Dog.js
+++ src/components/dog/Dog.js
@@ -1,9 +1,23 @@
 import Character from "../character/Character";
+import Sprite from "../sprite/Sprite";
 import { DOG_DIMENTIONS } from "../../config/constants";
+import { useSelector } from "react-redux";
 
 function Dog({frame, direction, top, left}) {
-    
-    return <Character 
+    const hunger = useSelector(state => state.rootReducer.game.stats.foodStat)
+    const walk = useSelector(state => state.rootReducer.game.stats.walkStat)
+
+    return (
+        <>
+        {( hunger < 50 || walk < 50 )&&
+                <Sprite
+                    image={( hunger > 20 && walk > 20 ) ? `/sprites/alert.png` : '/sprites/bigalert.png'}
+                    data={{ h: 32, w: 32, x: 0, y: 0 }}
+                    top={top - 30}
+                    left={left}
+                />
+            }
+        <Character 
             sprite={`/sprites/characters/pup.png`} 
             data={DOG_DIMENTIONS} 
             frame={frame} 
@@ -11,7 +25,8 @@ function Dog({frame, direction, top, left}) {
             top={top}
             left={left}
         />
-
+        </>
+    )
 }
 
 export default Dog;
