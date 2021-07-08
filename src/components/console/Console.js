5caa1881b25a07f9ec791b309377dcd952929244
raquel <raquelanaroman@gmail.com>
2021-07-07 21:03:45 -0400

    init console component

---
+++ src/components/console/Console.js
@@ -0,0 +1,13 @@
+import './style.css';
+
+function Console(){
+    return (
+        <div id="console-container">
+            <div id="console">
+                <p className="typewriter">&gt; This is a message for my haters.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Console;
