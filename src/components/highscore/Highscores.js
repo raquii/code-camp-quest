ccdbb065fe3d97b71d0ba5c3b0c868744fe799e5
raquel <raquelanaroman@gmail.com>
2021-07-08 21:16:35 -0400

    create highscores page

---
+++ src/components/highscore/Highscores.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import './style.css';
+
+function Highscores() {
+    const [scores, setScores] = useState(null);
+
+    useEffect(() => {
+
+        fetch("https://boiling-reaches-07903.herokuapp.com/highscores")
+            .then(r => r.json())
+            .then(data => setScores(data))
+
+    }, []);
+
+    if (!scores) {
+        return <h2>Waking up my heroku server...</h2>
+    }
+
+    const scoresTable = scores.map(score => {
+        return (
+            <tr key={score.id}>
+                <td>
+                    {score.id}
+                </td>
+                <td>
+                    {score.name}
+                </td>
+                <td>
+                    {score.score}
+                </td>
+            </tr>
+        )
+    })
+
+    return (
+        <>
+            <h1 id='highscore'>High Scores</h1>
+            <div id='highscore-div'>
+                <table id='highscore-table'>
+                    <tr>
+                        <th>Place</th>
+                        <th>Name</th>
+                        <th>Score</th>
+                    </tr>
+                    {scoresTable}
+                </table>
+            </div>
+        </>
+    )
+}
+
+export default Highscores;
